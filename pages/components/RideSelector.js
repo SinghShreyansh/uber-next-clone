28d5610f22f4697b9a35405cb95ebcf1d7f14239
Shreyansh Singh <2020.shreyansh.singh@ves.ac.in>
2022-02-14 14:56:33 +0530

    Change default export syntax in carList.js

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import tw from 'tailwind-styled-components'
-import { carList } from '../data/carList'
+import carList from '../data/carList'
 
 const RideSelector = () => {
     return (
